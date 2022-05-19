329db1e582ac47e91b5d573c5d87d8e7fd17a50a
Christian Gould <cagould@smu.edu>
2022-05-19 13:49:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal_webpage/precache-manifest.c9cc47d4f7e186c5ac19f6326c8031a2.js"
+  "/personal_webpage/precache-manifest.8cec96828ef5b837e292a2b66a90dcc0.js"
 );
 
 self.addEventListener('message', (event) => {
