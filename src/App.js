132299c982ffbf18611e6881ab286cf1b2e601c6
Christian Gould <cagould@smu.edu>
2022-06-15 14:55:30 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -7,20 +7,6 @@ import { NotFound } from "./404";
 import {default as Router} from "./routes";
 
 class App extends Component {
-  // render() {
-  //   return (
-  //     <React.Fragment>
-  //       <Router>
-  //         <Switch>
-  //           <Route exact path="/" component={Home3}/>
-  //           <Route exact path="/personal_webpage" component={Home3} />
-  //           <Route exact path="/blog" component={PersonalBlog} />
-  //           <Route component={NotFound} />
-  //         </Switch>
-  //       </Router>
-  //     </React.Fragment>
-  //   );
-  // }
   render() {
     return (
       <React.Fragment>
