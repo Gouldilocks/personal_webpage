9ac7eca49122adf39e533d1489bfe9841260f599
Christian Gould <cagould@smu.edu>
2022-06-15 20:25:55 -0500

    initial change

--- src/component/Personalblog/PersonalBlog.js
+++ src/component/Personalblog/PersonalBlog.js
@@ -13,6 +13,42 @@ export default function PersonalBlog({changeBlog}) {
       }}>
         Return to Website
       </Button>
+
+
+
+
+      <section className={`${aClass}`} id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7 d-flex align-items-center">
+              <Fade bottom cascade duration={1000}>
+                <div className="about_content">
+                  <h2 className="t_color">{jhonData.aboutme}</h2>
+                  <h6>{jhonData.role}</h6>
+                  <p>{jhonData.aboutdetails}</p>
+                  <Button
+                    onClick={
+                      () => {
+                        window.open(fileUrl, '_blank');
+                      }
+                    }
+                    style={{
+                      width: '10rem',
+                      height: '4rem',
+                    }}
+                    className="theme_btn active">
+                    Download Resume</Button>
+                </div>
+              </Fade>
+            </div>
+            <div className="col-lg-5">
+              <div className="about_img">
+                <Reveal effect="fadeInRight" duration={1500}><img src={require('../image/pictureofme.jpg')} alt="" /></Reveal>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
